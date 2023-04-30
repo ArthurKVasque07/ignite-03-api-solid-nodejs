3cac9e370673e557485b0e18de75463e9fa799fa
Arthur Kavagouth Vasque <arthurkvasque.eng@outlook.com>
2023-04-30 11:12:08 -0300

    feat: add AuthenticateUseCase

---
+++ src/use-cases/authenticate.ts
@@ -0,0 +1,39 @@
+import { IUsersRepository } from "@/repositories/interfaces/users-repository";
+import { InvalidCredentialsError } from "./errors/invalid-credentials-error";
+import { compare } from "bcryptjs";
+import { User } from "@prisma/client";
+
+interface AuthenticateUseCaseRequest {
+  email: string;
+  password: string;
+}
+
+interface AuthenticateUseCaseResponse {
+  user: User;
+}
+
+export class AuthenticateUseCase {
+  constructor(private usersRepository: IUsersRepository) {}
+
+  async execute({
+    email,
+    password,
+  }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new InvalidCredentialsError();
+    }
+
+    // variaveis boolean => leitura semantica - "is" "has" "does"
+    const doesPasswordMatches = await compare(password, user.password_hash);
+
+    if (!doesPasswordMatches) {
+      throw new InvalidCredentialsError();
+    }
+
+    return {
+      user,
+    };
+  }
+}
