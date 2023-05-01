b32160bb083f33c11acc6b91bd61eea1adce8f40
Arthur Kavagouth Vasque <arthurkvasque.eng@outlook.com>
2023-05-01 10:28:06 -0300

    feat: create authenticate

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,6 +1,11 @@
-import { FastifyInstance } from 'fastify'
-import { register } from './controllers/register'
+import { FastifyInstance } from "fastify";
+import { register } from "./controllers/register";
+import { authenticate } from "./controllers/authenticate";
 
 export async function appRoutes(app: FastifyInstance) {
-  app.post('/users', register)
+  app.post("/users", register);
+
+  // rota como session para ficar mais semântico,
+  // estou criando uma sessão, invés de estou criando uma autenticação
+  app.post("/session", authenticate);
 }
