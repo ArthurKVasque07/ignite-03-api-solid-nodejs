17c0ca031000f37724b95b451f5b0f9f9cc40e35
Arthur Kavagouth Vasque <arthurkvasque.eng@outlook.com>
2023-05-02 15:04:21 -0300

    feat: add distanceInMinutesFromCheckInCreation

--- src/tests/validate-check-in.spec.ts
+++ src/tests/validate-check-in.spec.ts
@@ -1,4 +1,5 @@
 import { InMemoryCheckInsRepository } from "@/repositories/in-memory/in-memory-check-ins-repository";
+import { LateCheckInValidationError } from "@/use-cases/errors/late-check-in-validation-error";
 import { ResourceNotFoundError } from "@/use-cases/errors/resource-not-found-error";
 import { ValidateCheckinUseCase } from "@/use-cases/validate-check-in";
 import { expect, describe, it, beforeEach, vi, afterEach } from "vitest";
@@ -38,5 +39,24 @@ describe("Validate Check-ins Use Case", () => {
         checkInId: "inexistent-check-in-id",
       })
     ).rejects.toBeInstanceOf(ResourceNotFoundError);
+  });
+
+  it("should not be able to validate  the check-in after 20 minutes of its creation", async () => {
+    vi.setSystemTime(new Date(2023, 0, 1, 13, 40));
+
+    const createdCheckIn = await checkInsRepository.create({
+      gym_id: "gym-01",
+      user_id: "user-01",
+    });
+
+    const twentyOneMinutesOnMiliseconds = 1000 * 60 * 21;
+
+    vi.advanceTimersByTime(twentyOneMinutesOnMiliseconds);
+
+    expect(() =>
+      sut.execute({
+        checkInId: createdCheckIn.id,
+      })
+    ).rejects.toBeInstanceOf(LateCheckInValidationError);
   });
 });
