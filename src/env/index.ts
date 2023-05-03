11aa86ab093d8e09c67ec307f17e842ecf8262f0
Arthur Kavagouth Vasque <arthurkvasque.eng@outlook.com>
2023-05-03 08:10:31 -0300

    add fastify jwt

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,18 +1,19 @@
-import 'dotenv/config';
-import { z } from 'zod';
+import "dotenv/config";
+import { z } from "zod";
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
-  PORT: z.coerce.number().default(3333)
-})
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+  JWT_SECRET: z.string(),
+  PORT: z.coerce.number().default(3333),
+});
 
 // safe parse valida process.env para ver se possui as infos de envSchema
 const _env = envSchema.safeParse(process.env);
 
-if(_env.success === false) {
-  console.log('Invalid envirnoment variables', _env.error.format());
+if (_env.success === false) {
+  console.log("Invalid envirnoment variables", _env.error.format());
 
-  throw new Error('Invalid envirnoment variables 😭');
+  throw new Error("Invalid envirnoment variables 😭");
 }
 
 export const env = _env.data;
