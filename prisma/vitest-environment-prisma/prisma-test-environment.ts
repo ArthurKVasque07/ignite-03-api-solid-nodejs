4d014d844bf9cb6ccd01e8ea7107ca5d953d3455
Arthur Kavagouth Vasque <arthurkvasque.eng@outlook.com>
2023-05-03 09:37:35 -0300

    feat: add prisma-test-environment

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,13 +1,42 @@
+import { PrismaClient } from "@prisma/client";
+import "dotenv/config";
+import { execSync } from "node:child_process";
+import { randomUUID } from "node:crypto";
 import { Environment } from "vitest";
+
+// DATABASE_URL="postgresql://docker:docker@localhost:5432/apisolid?schema=public"
+
+const prisma = new PrismaClient();
+
+function generateDatabaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please provide a DATABASE_URL environment variable");
+  }
+
+  const url = new URL(process.env.DATABASE_URL);
+
+  url.searchParams.set("schema", schema);
+
+  return url.toString();
+}
 
 export default <Environment>{
   name: "prisma",
   async setup() {
-    console.log("setup");
+    const schema = randomUUID();
+    const databaseURL = generateDatabaseURL(schema);
+
+    process.env.DATABASE_URL = databaseURL;
+
+    // utiliza o deploy porque ele pula a etapa de verificação das migrations, apenas executa uma a uma
+    execSync("npx prisma migrate deploy");
 
     return {
-      teardown() {
-        console.log("Teardown");
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          // cascade garante que tudo sera apagado na casacata, ex FK, Index
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        );
       },
     };
   },
