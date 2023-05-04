1123f766c633c4b6ae309215be641022318833e2
Arthur Kavagouth Vasque <arthurkvasque.eng@outlook.com>
2023-05-04 08:51:11 -0300

    finish test

--- src/tests/e2e-tests/create-check-in.spec.ts
+++ src/tests/e2e-tests/create-check-in.spec.ts
@@ -14,7 +14,7 @@ describe("Create Check-in (e2e)", () => {
   });
 
   it("sould be able to create a check-in", async () => {
-    const { token } = await createAndAuthenticateUser(app);
+    const { token } = await createAndAuthenticateUser(app, true);
 
     const { id } = await prisma.gym.create({
       data: {
